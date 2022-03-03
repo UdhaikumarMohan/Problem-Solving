ee326d68bd27e054a6e85f6da3cda4f9fd3769be
Udhaikumar Mohan <udhaikumar4@gmail.com>
2022-03-03 18:50:14 +0530

    Third Problem Solved

---
+++ Volume.py
@@ -0,0 +1,23 @@
+#Find the volume of the rectangular water tank:
+#Variables Length(l), Width(w), Height(h) and Volume(vol) in meters.
+
+#Define the function:
+def Water_tank(l, w, h):
+
+#Negative values are not allowed here, so we are going to ignore it using if condition:
+    if (l<=-1) or (w<=-1) or (h<=-1):
+        print("Negative values are not allowed, Please give a valid input")
+        
+    else:
+#Apply the Mathematical formula here:
+        vol = (l*w*h)
+        print("The volume of the water tank is: ", str(vol) + " m^3")
+       
+#User Input:
+print("Inputs has been taken in unit, Meter(m)")
+l = float(input("Enter the Length of the water tank: "))
+w = float(input("Enter the Width of the water tank: "))
+h = float(input("Enter the Height of the water tank: "))
+
+#Calling the function:
+Water_tank(l, w, h)
