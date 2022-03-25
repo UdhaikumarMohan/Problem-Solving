75cdc8daf0d32ccefd60b630e2e387fa94b0f6e2
Udhaikumar Mohan <udhaikumar4@gmail.com>
2022-03-25 10:15:21 +0530

    14th problem solved

--- Rand_Num.py
+++ Rand_Num.py
@@ -6,9 +6,8 @@ import random
 def Rand():
     Random_Number_1 = random.randrange(1, 100)
     Random_Number_2 = random.randint(1, 100)
-    Random_Number_3 = random.next(1, 100)
-
-    print(Random_Number_1, Random_Number_2, Random_Number_3)
+   
+    print(Random_Number_1, Random_Number_2)
 
 # Call the function:
 Rand()
