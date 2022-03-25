9579960bcc063b32b817df349a42137aad1a551f
Udhaikumar Mohan <udhaikumar4@gmail.com>
2022-03-25 10:10:57 +0530

    14th Problem

---
+++ Rand_Num.py
@@ -0,0 +1,14 @@
+# Generate Random number (1 - 100):
+
+import random
+
+# Define the function
+def Rand():
+    Random_Number_1 = random.randrange(1, 100)
+    Random_Number_2 = random.randint(1, 100)
+    Random_Number_3 = random.next(1, 100)
+
+    print(Random_Number_1, Random_Number_2, Random_Number_3)
+
+# Call the function:
+Rand()
